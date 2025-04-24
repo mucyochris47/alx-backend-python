261214b9cd61e8d9ffe0dea420626fdca21b332b
mucyochris47 <mucyochris250@gmail.com>
2025-04-24 16:55:42 -0700

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Write a type-annotated function floor which takes a float n as argument
+    and returns the floor of the float.
+"""
+import math
+""" the floor function from math modulev """
+
+
+def floor(n: float) -> int:
+    """ Write a type-annotated function floor which takes a float n as argument
+        and returns the floor of the float.
+    """
+    return math.floor(n)

b498b0e787333543b1531162bdedd61ee08b2eef
mucyochris47 <mucyochris250@gmail.com>
2025-04-24 17:08:53 -0700

    task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Augment the following code with the correct duck-typed annotations: """
+""" # The types of the elements of the input are not know """
+
+
+def safe_first_element(lst):
+    """ Augment the following code with the correct duck-typed annotations: """
+    """ # The types of the elements of the input are not know """
+    if lst:
+        return lst[0]
+    else:
+        return None

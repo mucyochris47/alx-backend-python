a252c561fbced1410511afe533ec659e1ab613df
mucyochris47 <mucyochris250@gmail.com>
2025-04-24 17:07:57 -0700

    task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Annotate the below function’s parameters and return values
+    with the appropriate types
+"""
+
+
+def element_length(lst):
+    """ Annotate the below function’s parameters and return values
+        with the appropriate types
+    """
+    return [(i, len(i)) for i in lst]
